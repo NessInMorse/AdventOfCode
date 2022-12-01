08238d49dd9d36119c431cace7739c77c7ea34bb
Marc <56824499+NessInMorse@users.noreply.github.com>
2022-12-01 06:32:24 +0100

    Create dag1_2.py

---
+++ 2022/Dag1/dag1_2.py
@@ -0,0 +1,22 @@
+def openFile():
+        infile = open("input.txt", "r")
+        raw = infile.read().split("\n")
+        infile.close()
+        return raw
+
+
+def main():
+        raw = openFile()
+        data = [int(raw[0])]
+        for i in range(1, len(raw)):
+                if raw[i] != "":
+                        data[-1] += int(raw[i])
+                else:
+                        data.append(0)
+        s = sorted(data)
+        print(s[-1] + s[-2] + s[-3])
+
+main()
+
+
+
